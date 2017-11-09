c8d5e7e98473f857adf61de3d877d67618da3c82
Cara Kane <cara.kane@gmail.com>
2017-11-09 16:18:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,5 @@ function deleteFromDriverByKey(driver, key) {
 }
 
 function destructivelyDeleteFromDriverByKey(driver, key) {
-   delete driver.key
-   return driver
+  return delete driver.key
 }
